2217b12d29786017a01507b386ab86c84809b903
jjbeto <jjbeto@gmail.com>
2022-12-15 14:28:16 -0300

    2022 day12 p1

---
+++ 2022/day12/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jjbeto/adventofcode/aoc"
+	"sort"
+)
+
+var (
+	lines  []string
+	xLimit int
+	yLimit int
+)
+
+func part1() int {
+	var start, end aoc.Coordinate
+	heightMap := make(map[aoc.Coordinate]rune)
+	for x, row := range lines {
+		for y, height := range row {
+			if height == 'S' {
+				start = aoc.Coordinate{X: x, Y: y}
+				height = 'a'
+			}
+			if height == 'E' {
+				end = aoc.Coordinate{X: x, Y: y}
+				height = 'z'
+			}
+			heightMap[aoc.Coordinate{X: x, Y: y}] = height
+		}
+	}
+
+	toVisit := []aoc.Coordinate{start}
+	visited := make(map[aoc.Coordinate]bool)
+	distance := map[aoc.Coordinate]int{
+		start: 0,
+	}
+	for {
+		if len(toVisit) == 0 {
+			break
+		}
+
+		curr := toVisit[0]
+		visited[curr] = true
+		toVisit = toVisit[1:]
+
+		if curr.Equals(end) {
+			break
+		}
+
+		for _, next := range curr.Adjacent(xLimit, yLimit) {
+			if !visited[next] && heightMap[next]-heightMap[curr] <= 1 {
+				if distance[next] == 0 {
+					toVisit = append(toVisit, next)
+					distance[next] = distance[curr] + 1
+				}
+			}
+		}
+		sort.Slice(toVisit, func(a, b int) bool {
+			return distance[toVisit[a]] < distance[toVisit[b]]
+		})
+	}
+
+	return distance[end]
+}
+
+func main() {
+	lines = aoc.ReadLines("2022/day12", "data.txt", aoc.ToString)
+	xLimit, yLimit = len(lines), len(lines[0])
+
+	fmt.Println("part1", part1())
+	//fmt.Println("part2", part2(lowPoints))
+}
