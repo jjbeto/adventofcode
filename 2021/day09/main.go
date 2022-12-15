8b13e60d980d52cb177044a023c6f124eb7d404a
jjbeto <jjbeto@gmail.com>
2022-12-15 14:28:00 -0300

    update 2021/day09 to use helper

--- 2021/day09/main.go
+++ 2021/day09/main.go
@@ -8,8 +8,7 @@ import (
 )
 
 var (
-	lines            []string
-	nColumns, nLines int
+	lines []string
 )
 
 func part1() (total int, lowPoints []string) {
@@ -42,57 +41,13 @@ func part1() (total int, lowPoints []string) {
 	return
 }
 
-type coord struct {
-	x int
-	y int
-}
-
-func contains(array []coord, elem coord) bool {
-	for _, each := range array {
-		if each == elem {
-			return true
-		}
-	}
-	return false
-}
-
-func (c *coord) isValid() bool {
-	xValid := c.x > -1 && c.x < nLines
-	yValid := c.y > -1 && c.y < nColumns
-	return xValid && yValid
-}
-
-func adjacent(root coord) (coords []coord) {
-	coords = append(coords,
-		coord{x: root.x + 1, y: root.y}, // up
-		coord{x: root.x - 1, y: root.y}, // down
-		coord{x: root.x, y: root.y - 1}, // left
-		coord{x: root.x, y: root.y + 1}, // right
-	)
-	return
-}
-
-func dfs(root coord, visited []coord) []coord {
-	if lines[root.x][root.y:root.y+1] == "9" || contains(visited, root) {
-		return visited
-	}
-
-	visited = append(visited, root)
-	for _, next := range adjacent(root) {
-		if next.isValid() {
-			visited = dfs(next, visited)
-		}
-	}
-	return visited
-}
-
 func part2(lowPoints []string) int {
 	var numbers aoc.SortableArray[int]
 	for _, root := range lowPoints {
 		split := strings.Split(root, ",")
 
-		root := coord{x: aoc.ToIntBase10(split[0]), y: aoc.ToIntBase10(split[1])}
-		visited := dfs(root, []coord{})
+		root := aoc.Coordinate{X: aoc.ToIntBase10(split[0]), Y: aoc.ToIntBase10(split[1])}
+		visited := aoc.DFS(lines, "9", root, []aoc.Coordinate{})
 		numbers = append(numbers, len(visited))
 	}
 
@@ -102,7 +57,6 @@ func part2(lowPoints []string) int {
 
 func main() {
 	lines = aoc.ReadLines("2021/day09", "data.txt", aoc.ToString)
-	nColumns, nLines = len(lines[0]), len(lines)
 
 	i, lowPoints := part1()
 	fmt.Println("part1", i)
