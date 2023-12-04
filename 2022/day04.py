4785efe576bd5f5daad5c9973ac22246f9cfa5ac
jjbeto <jjbeto@gmail.com>
2023-12-04 21:35:50 +0100

    removing inputs

--- 2022/day04.py
+++ 2022/day04.py
@@ -4,18 +4,15 @@ import utils
 
 raw = get_data(day=4, year=2022)
 
-total = 0
+answer_1 = 0
+answer_2 = 0
+
 for line in raw.splitlines():
     x1, y1, x2, y2 = utils.extract_int('%-%,%-%', line)
     if (x1 <= x2 <= y1 and x1 <= y2 <= y1) or (x2 <= x1 <= y2 and x2 <= y1 <= y2):
-        total += 1
-
-print(f'Answer 1: {total}')
-
-total = 0
-for line in raw.splitlines():
-    x1, y1, x2, y2 = utils.extract_int('%-%,%-%', line)
+        answer_1 += 1
     if x1 <= x2 <= y1 or x1 <= y2 <= y1 or x2 <= x1 <= y2 or x2 <= y1 <= y2:
-        total += 1
+        answer_2 += 1
 
-print(f'Answer 2: {total}')
+print(f'Answer 1: {answer_1}')
+print(f'Answer 2: {answer_2}')
